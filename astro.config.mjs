a5f4a7cd200380111309aca0a27689b687b84ade
budhihartati12-sketch <budhihartati12@guru.smp.belajar.id>
2025-09-18 00:58:29 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://itif.github.io',
+	site: 'https://itif-syuhada.github.io',
 	integrations: [mdx(), sitemap()],
 	base: '/',
 });
